642085e3c72ac344d594ac987ccb5b0cfedfa44b
Jack Dietrich <jed123@rogers.com>
2025-07-12 13:02:54 -0400

    Added 40khz carrier frequency

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,43 @@
 #include <Arduino.h>
-#define irLED 2 //irLED on pin 2
+#define irLED 16 //irLED on pin 2
+
+//vars
+unsigned long delayStart = 0;
+
+
 // put function declarations here:
+void high(int onTime){ //ontime is microseconds
+  delayStart = micros();//start timer
+  while ((micros() - delayStart) < onTime) //while current time minus when we started is less than specified ontime
+  {
+    digitalWrite(irLED,HIGH);
+    delayMicroseconds(25);//40khz
+    digitalWrite(irLED,LOW);
+  }
+  digitalWrite(irLED,LOW);//turn off once time has expired
+} 
+
+
+void LH(){//long high function
+  high(1000);
+  delayMicroseconds(500);
+}
+
+void SH(){
+  high(500);
+
+  delayMicroseconds(500);
+
+}
+
+void start(){
+  high(2000);
+  delayMicroseconds(500);
+
+}
+
+
+
 void on(){
   start();
   LH();
@@ -21,24 +58,6 @@ void on(){
 
 };
 
-void LH(){//long high function
-  digitalWrite(irLED,HIGH);
-  delay(1);//delay 1 ms
-  digitalWrite(irLED,LOW);
-}
-
-void SH(){
-  digitalWrite(irLED,HIGH);
-  delayMicroseconds(500);//delay 500 microseconds
-  digitalWrite(irLED,LOW);
-}
-
-void start(){
-  digitalWrite(irLED,HIGH);
-  delay(2);//delay 500 microseconds
-  digitalWrite(irLED,LOW);
-}
-
 void setup() {
   // put your setup code here, to run once:
   pinMode(irLED,OUTPUT);
@@ -46,6 +65,6 @@ void setup() {
 
 void loop() {
   on();
-  delay(100);
+  delay(20);//20ms delay before retrying
 }
 
